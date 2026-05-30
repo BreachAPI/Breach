ca7b8ca8a19fbf200ead85d3ef26c3c2e75b116f
Leemyy <yunomanics@live.de>
2026-05-30 23:55:14 +0200

    feat: mod loading and dependency ordering

--- src/API/Loader/Loader.cs
+++ src/API/Loader/Loader.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Breach.Tools;
 
@@ -13,12 +15,13 @@ namespace Breach
         public static readonly string PathGameFolder;
         public static readonly string PathModsFolder;
 
-        public static List<BreachModule> Modules = new List<BreachModule>();
+        public static ImmutableArray<BreachMod> Mods { get; private set; } = ImmutableArray<BreachMod>.Empty;
+        public static ImmutableArray<BreachModule> Modules { get; private set; } = ImmutableArray<BreachModule>.Empty;
 
         static Loader()
         {
             PathGameFile = Path.GetFullPath(typeof(OuterBeyond.THGame).Assembly.Location);
-            PathGameFolder = Path.GetDirectoryName(PathGameFile);
+            PathGameFolder = Path.GetDirectoryName(PathGameFile)!;
             PathModsFolder = Path.Combine(PathGameFolder, "Mods");
 
             AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
@@ -32,9 +35,9 @@ namespace Breach
 
             var requestingFile = new FileInfo(e.RequestingAssembly.Location);
             var requestingDir = requestingFile.Directory;
+            if (requestingDir is null) return null;
 
             var assemblyName = new AssemblyName(e.Name);
-
             var targetPath = Path.Combine(requestingDir.FullName, assemblyName + ".dll");
 
             if (!File.Exists(targetPath))
@@ -47,47 +50,163 @@ namespace Breach
 
         public static void Load()
         {
-            if (Directory.Exists(PathModsFolder))
+            if (!Directory.Exists(PathModsFolder))
             {
-                var info = new DirectoryInfo(PathModsFolder);
-                foreach (var file in info.EnumerateFiles("*.Mod.dll", SearchOption.TopDirectoryOnly))
-                {
-                    var assembly = Assembly.LoadFrom(file.FullName);
+                // TODO: Log Mods folder missing and create it
+                return;
+            }
+
+            // Find and parse all metadata files
+            var toLoad = FindMods();
+            // Ensure all mod Ids are unique and all dependencies are present
+            VerifyMetadata(toLoad);
+            toLoad = OrderModsByDependencies(toLoad);
+
+            var loadedMods = LoadMods(toLoad);
+            Mods = loadedMods.ToImmutableArray();
+            var loadedModules = loadedMods.SelectMany(m => m.Modules).ToImmutableArray();
+            Modules = loadedModules;
+            
+            foreach (var module in loadedModules) {
+                module.Initialize();
+            }
+        }
+
+        private static List<BreachModHandle> FindMods() {
+            var list = new List<BreachModHandle>();
+
+            foreach (var modDir in Directory.EnumerateDirectories(PathModsFolder)) {
+                var metadataFile = new FileInfo(Path.Combine(modDir, "mod.json"));
+                if (!metadataFile.Exists) continue;
+
+                list.Add(new BreachModHandle {
+                    Metadata = ParseMetadata(metadataFile),
+                    Path = modDir
+                });
+            }
+
+            return list;
+        }
+
+        public static BreachModMetadata ParseMetadata(FileInfo metadataFile) {
+            // TODO: load metadata json
+            return new BreachModMetadata();
+        }
+
+        private static void VerifyMetadata(List<BreachModHandle> modList) {
+            bool hasIdConflicts = false;
+            var knownIds = new Dictionary<string, BreachModHandle>();
+            foreach (var modHandle in modList) {
+                if (knownIds.TryGetValue(modHandle.Metadata.Id, out var other)) {
+                    hasIdConflicts = true;
+                    // TODO: Log mod conflict (including file paths)
+                }
+
+                knownIds.Add(modHandle.Metadata.Id, modHandle);
+            }
+
+            if (hasIdConflicts) {
+                throw new Exception("mod load error: multiple mods with same id");
+            }
+
+            var missingDependencies = new List<Tuple<string, BreachModHandle>>();
+            foreach (var modHandle in modList) {
+                foreach (var dependency in modHandle.Metadata.Dependencies) {
+                    if (!knownIds.ContainsKey(dependency)) {
+                        missingDependencies.Add(new(dependency, modHandle));
+                        // TODO: Log missing dependency (mod ID, dep ID)
+                    }
+                }
+            }
+
+            if (missingDependencies.Count > 0) {
+                throw new Exception("mod load error: missing dependencies");
+            }
+        }
+
+        private static List<BreachModHandle> OrderModsByDependencies(List<BreachModHandle> modList) {
+            var loadOrder = new List<BreachModHandle>();
+            var processedMods = new HashSet<string>();
+            var remainingMods = new List<BreachModHandle>(modList.Count);
+
+            // Default to loading in order of Id, to ensure consistent load order
+            remainingMods.AddRange(modList.OrderBy(m => m.Metadata.Id));
+
+            while (loadOrder.Count < remainingMods.Count) {
+                int orderedCountBefore = loadOrder.Count;
+                for (int i = 0; i < remainingMods.Count; i++) {
+                    // We set entries to null when we moved them to loadOrder, so skip nulls
+                    if (remainingMods[i] == null) continue;
+
+                    var currentMod = remainingMods[i];
+                    bool allDependenciesPresent = true;
+                    foreach (var dependency in currentMod.Metadata.Dependencies) {
+                        if (!processedMods.Contains(dependency)) {
+                            allDependenciesPresent = false;
+                        }
+                    }
+
+                    if (allDependenciesPresent) {
+                        loadOrder.Add(currentMod);
+                        processedMods.Add(currentMod.Metadata.Id);
+                        // Don't remove so we don't have to worry about shifting indices.
+                        remainingMods[i] = null;
+                    }
+                }
+
+                if (loadOrder.Count == orderedCountBefore) {
+                    // We didn't find a single mod without missing dependencies.
+                    // This means there is a dependency loop (or multiple)!
+                    // TODO: Log all mods still in remainingMods
+                    throw new Exception("mod load error: circular dependency");
+                }
+            }
+
+            return loadOrder;
+        }
+
+        private static List<BreachMod> LoadMods(List<BreachModHandle> modList) {
+            var loadedModList = new List<BreachMod>();
+            var loadOrder = modList.Select(m => m.Metadata).ToImmutableList();
+
+            foreach (var mod in modList) {
+                var moduleList = new List<BreachModule>();
+
+                foreach (var file in mod.Metadata.Assemblies) {
+                    var assembly = Assembly.LoadFrom(Path.Combine(mod.Path, file));
 
                     foreach (var module in assembly.GetModules())
                     {
                         foreach (var type in module.GetTypes())
                         {
-                            // Skip types that are not marked with the [BreachModule] attribute
-                            var attr = type.GetCustomAttribute<BreachModuleAttribute>();
-                            if (attr == null) continue;
-
                             // Skip types that do not derive from BreachModule
-                            bool hasBreachModuleParent = false;
-                            var parentType = type.BaseType;
-                            while (parentType != null)
-                            {
-                                if (parentType == typeof(BreachModule))
-                                {
-                                    hasBreachModuleParent = true;
-                                    break;
-                                }
+                            if (!typeof(BreachModule).IsAssignableFrom(type)) continue;
+                            // Skip types that are not marked with the [AutoLoad] attribute
+                            var attr = type.GetCustomAttribute<AutoLoadAttribute>();
+                            if (attr is null) continue;
 
-                                parentType = type.BaseType;
-                            }
-                            if (!hasBreachModuleParent) continue;
-
-                            // Create instance and go to town
-                            BreachModule instance = (BreachModule)Activator.CreateInstance(type);
-                            instance.Logger = Log.GetModLogger(attr.LoggerName ?? file.Name, attr.HasOwnLogFile);
-                            Modules.Add(instance);
-                            instance.Initialize();
+                            // Instantiate BreachModule
+                            var instance = (BreachModule)Activator.CreateInstance(type);
+                            instance.Logger = Log.GetModLogger(attr.LoggerName ?? file, attr.HasOwnLogFile);
+                            moduleList.Add(instance);
                         }
                     }
                 }
+
+                for (int i = 0; i < moduleList.Count; i++) {
+                    var module = moduleList[i];
+                    var register = module.RegisterModules(loadOrder);
+                    moduleList.AddRange(register);
+                }
+
+                loadedModList.Add(new() {
+                    Metadata = mod.Metadata,
+                    Path = mod.Path,
+                    Modules = moduleList.ToImmutableArray()
+                });
             }
 
-            Directory.SetCurrentDirectory(PathGameFolder);
+            return loadedModList;
         }
     }
 }
--- src/API/Loader/Mod.cs
+++ src/API/Loader/Mod.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using log4net;
 using NuGet.Versioning;
 
 namespace Breach
 {
     /// <summary>
-    /// Marks the class as a Breach module to be loaded
+    /// Marks the class as a Breach module to be loaded at startup
     /// </summary>
-    public class BreachModuleAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Class)]
+    public class AutoLoadAttribute : Attribute
     {
         public bool HasOwnLogFile { get; set; } = false;
         public string LoggerName { get; set; } = null;
@@ -17,73 +19,154 @@ namespace Breach
     /// <summary>
     /// Represents a loadable Breach code module
     /// </summary>
-    public class BreachModule
+    public abstract class BreachModule
     {
         public ILog Logger { get; internal set; }
 
         /// <summary>
-        /// Override to add initialization code to your mod (namely, add hooks)
+        /// Override to load optional modules (integration with other mods).
+        /// <br/>
+        /// This method is called <i>before</i> <see cref="Initialize"/>,
+        /// it allows mods to dynamically register additional modules.
+        /// <br/>
+        /// This can be used to implement integration with other mods,
+        /// which are not a direct dependency of this mod.
+        /// Inspect the <paramref name="loadedMods"/> list to determine
+        /// which other mods have been loaded.
+        /// </summary>
+        /// <param name="loadedMods">list of all mods which have been loaded, in load order</param>
+        /// <returns>A list of additional modules that this mod wishes to register.
+        /// <br/>
+        /// or <c>null</c>, if no additional modules should be registered.</returns>
+        public virtual IEnumerable<BreachModule> RegisterModules(IReadOnlyList<BreachModMetadata> loadedMods) {
+            return null;
+        }
+
+        /// <summary>
+        /// Override to add initialization code to your mod (namely, add hooks).
+        /// <br/>
+        /// Module initialization happens immediately after
+        /// <see cref="RegisterModules"/> has been called on all mods,
+        /// but <b>before</b> the game itself is initialized.
+        /// <br/>
+        /// Mods are initialized in load order, a mod is always
+        /// loaded <b>after</b> all of its dependencies.
+        /// Mods that do not depend on each other may be loaded
+        /// in any order.
         /// </summary>
         public virtual void Initialize()
         {
         }
 
         /// <summary>
-        /// Override to undo initialization code and dispose of system resources (namely, dispose of hooks)
+        /// Override to run logic after the game is fully initialized.
+        /// <br/>
+        /// The post-initialization step happens <b>after</b>
+        /// the game has been initialized,
+        /// but <b>before</b> the core game loop is started.
+        /// <br/>
+        /// Mods are initialized in load order, a mod is always
+        /// loaded <b>after</b> all of its dependencies.
+        /// Mods that do not depend on each other may be loaded
+        /// in any order.
+        /// </summary>
+        public virtual void PostInitialize()
+        {
+        }
+
+        /// <summary>
+        /// Override to undo initialization code and dispose of system resources (namely, dispose of hooks).
         /// </summary>
         public virtual void Deinitialize()
         {
         }
 
         /// <summary>
-        /// Override to do additional content loading work
+        /// Override to do additional content loading work.
         /// </summary>
         public virtual void LoadContent()
         {
         }
 
         /// <summary>
-        /// Override to do additional content unloading work
+        /// Override to do additional content unloading work.
         /// </summary>
         public virtual void UnloadContent()
         {
         }
     }
 
-    public class BreachMod
-    {
-        public BreachModMetadata Metadata;
-        public List<BreachModule> Modules;
-    }
-
     /// <summary>
     /// Serializable mod metadata
     /// </summary>
-    public struct BreachModMetadata
+    public class BreachModMetadata
     {
         /// <summary>
-        /// Unique identifier for the mod
+        /// Unique identifier of this mod.
+        /// <br/>
+        /// A mod's ID can be referenced in the
+        /// <see cref="Dependencies"/> of another mod.
         /// </summary>
-        public string Id { get; set; }
+        public string Id { get; init; }
 
         /// <summary>
-        /// Display name for the mod
+        /// Display name for this mod.
+        /// <br/>
+        /// Used in menus and settings.
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; init; }
 
         /// <summary>
-        /// Version of the mod used
+        /// Version of this mod used.
+        /// <br/>
+        /// The version is determined by the mod author.
         /// </summary>
-        public SemanticVersion Version;
+        public SemanticVersion Version { get; init; }
 
         /// <summary>
-        /// Filenames for the mod's assemblies
+        /// Filenames of this mod's assemblies.
         /// </summary>
-        public string[] Assemblies;
+        public ImmutableArray<string> Assemblies { get; init; } = ImmutableArray<string>.Empty;
 
         /// <summary>
-        /// IDs of dependency mods
+        /// IDs of other mods which this mod depends on.
+        /// <br/>
+        /// Breach ensures that all of a mod's dependencies are present,
+        /// and will produce an error if any are missing.
+        /// <br/>
+        /// On startup, all mods are sorted such that each mod
+        /// is only initialized after <b>all</b> its dependencies
+        /// have been initialized.
         /// </summary>
-        public string[] Dependencies;
+        public ImmutableArray<string> Dependencies { get; init; } = ImmutableArray<string>.Empty;
+    }
+
+    public class BreachMod {
+        /// <summary>
+        /// Contents of this mod's <c>mod.json</c>.
+        /// </summary>
+        public BreachModMetadata Metadata { get; init; }
+
+        /// <summary>
+        /// Path of the folder which this mod was loaded from.
+        /// </summary>
+        public string Path { get; init; }
+
+        /// <summary>
+        /// List of all modules that have been registered by this mod.
+        /// </summary>
+        public ImmutableArray<BreachModule> Modules { get; init; }
+    }
+
+    internal class BreachModHandle {
+        /// <summary>
+        /// Contents of this mod's <c>mod.json</c>.
+        /// </summary>
+        public BreachModMetadata Metadata { get; init; }
+
+        /// <summary>
+        /// Path of the folder which this mod was loaded from.
+        /// </summary>
+        public string Path { get; init; }
     }
 }
